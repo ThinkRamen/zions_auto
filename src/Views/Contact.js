31e5e8c915e7749ed0d84d9a1ba94684672a2f7f
Alejandro <ThinkRamen@gmail.com>
2023-04-26 22:23:26 -0700

    form labels to contact page

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,7 +1,16 @@
+import { Form, FormLabel } from 'react-bootstrap'
 function Contact() {
 	return (
 		<>
 			<h1>Contact</h1>
+			<Form>
+				<FormLabel>Contact Request</FormLabel>
+				<FormLabel>First Name:</FormLabel>
+				<FormLabel>Last Name:</FormLabel>
+				<FormLabel>Email:</FormLabel>
+				<FormLabel>Phone Number:</FormLabel>
+				<FormLabel>Comments</FormLabel>
+			</Form>
 		</>
 	)
 }
